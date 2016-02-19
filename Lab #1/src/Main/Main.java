8fbb0d688c669e677e36c848c2d9e5e0b924d164
Andrew <Andrew@wifi-roaming-128-4-228-118.host.udel.edu>
2016-02-19 15:42:49 -0500

    Revised

--- Lab #1/src/Main/Main.java
+++ Lab #1/src/Main/Main.java
@@ -8,25 +8,27 @@ public class Main {
 		System.out.println("Enter how many years you plan to work: ");
 		years_to_work = user_input.nextInt();
 		
-		double average_return;
+		double annual_return;
 		System.out.println("Enter the expected average return on an investement: ");
-		average_return = user_input.nextDouble();
+		annual_return = user_input.nextDouble();
 		
-		int draw;
-		System.out.println("Enter the expected average return on an investement: ");
-		draw = user_input.nextInt();
+		int years_retired;
+		System.out.println("Enter the amount of years retired : ");
+		years_retired = user_input.nextInt();
 		
 		double required_income;
-		System.out.println("Enter the investement: ");
+		System.out.println("Enter your required income: ");
 		required_income = user_input.nextDouble();
 		
 		double SSI_income;
-		System.out.println("Enter the expected average return on an investement: ");
+		System.out.println("Enter the expected SSI income: ");
 		SSI_income = user_input.nextDouble();
 		
 		user_input.close();
-		double total_saving = (required_income-SSI_income)*((1-(1/(Math.pow(1+(average_return/100)/12,years_to_work*12 )))))/((average_return/100)/12);
-		double save_monthly = total_saving*(((average_return/100)/12)/((Math.pow(1+(average_return/100)/12, years_to_work*12))-1));
+		double total_saving = (required_income-SSI_income)*((1-(1/(Math.pow(1+(annual_return/100)/12,years_to_work*12 )))))/((annual_return/100)/12);
+		double save_monthly = total_saving*(((annual_return/100)/12)/((Math.pow(1+(annual_return/100)/12, years_to_work*12))-1));
+		System.out.printf("%.2f",total_saving, "\n");
+		System.out.printf("%.2f",save_monthly, "\n");
 	}
 }
 
