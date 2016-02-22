efdf25ac3cc729dfb4cc636fbd7848f6860e37b4
Peter Delaney <pdelaney@udel.edu>
2016-02-22 01:55:40 -0500

    took the formulas and put them into methods, if we don't want to use it thats fine, just wanted to have that option there

--- Lab #1/src/Main/Main.java
+++ Lab #1/src/Main/Main.java
@@ -4,31 +4,35 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner user_input = new Scanner(System.in);
-		int years_to_work;
+
 		System.out.println("Enter how many years you plan to work: ");
-		years_to_work = user_input.nextInt();
-		
-		double annual_return;
+		int years_to_work = user_input.nextInt();
+
 		System.out.println("Enter the expected average return on an investement: ");
-		annual_return = user_input.nextDouble();
+		double annual_return = user_input.nextDouble();
 		
-		int years_retired;
 		System.out.println("Enter the amount of years retired : ");
-		years_retired = user_input.nextInt();
-		
-		double required_income;
+		int years_retired = user_input.nextInt();
+
 		System.out.println("Enter your required income: ");
-		required_income = user_input.nextDouble();
-		
-		double SSI_income;
+		double required_income = user_input.nextDouble();
+
 		System.out.println("Enter the expected SSI income: ");
-		SSI_income = user_input.nextDouble();
+		double SSI_income = user_input.nextDouble();
 		
 		user_input.close();
-		double total_saving = (required_income-SSI_income)*((1-(1/(Math.pow(1+(annual_return/100)/12,years_to_work*12 )))))/((annual_return/100)/12);
-		double save_monthly = total_saving*(((annual_return/100)/12)/((Math.pow(1+(annual_return/100)/12, years_to_work*12))-1));
+		double total_saving = total_saving(required_income, SSI_income, annual_return, years_to_work);
+		double save_monthly = save_monthly(total_saving, annual_return, years_to_work);
 		System.out.printf("%.2f",total_saving, "\n");
 		System.out.printf("%.2f",save_monthly, "\n");
+	}
+	public static double total_saving(double required_income,double SSI_income,double annual_return,int years_to_work) {
+		double total_saving = (required_income-SSI_income)*((1-(1/(Math.pow(1+(annual_return/100)/12, years_to_work*12 )))))/((annual_return/100)/12);
+		return total_saving;
+	}
+	public static double save_monthly(double total_saving,double annual_return,int years_to_work) {
+		double save_monthly = total_saving*(((annual_return/100)/12)/((Math.pow(1+(annual_return/100)/12, years_to_work*12))-1));
+		return save_monthly;
 	}
 }
 
