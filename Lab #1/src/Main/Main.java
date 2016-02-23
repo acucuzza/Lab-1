d5469955b3e8a6804c499f3db02086928eb24f05
David Heck <dheck@udel.edu>
2016-02-23 01:46:42 -0500

    Finished Lab. (Needs names)

--- Lab #1/src/Main/Main.java
+++ Lab #1/src/Main/Main.java
@@ -1,23 +1,30 @@
 package Main;
 import java.util.Scanner;
 
+//Names
+
 public class Main {
-	public static void main(String[] args) {
-		user();
-		
-		
+	public static void main(String[] args) 
+	{
+		user();	//Call the user method.
 	}
-	public static void user() {
+	public static void user() 
+	{
+		//Collect user input to feed into calculations
+		
 		Scanner user_input = new Scanner(System.in);
 
 		System.out.println("Enter how many years you plan to work: ");
-		int years_to_work = user_input.nextInt();
+		double years_to_work = user_input.nextDouble();
 
-		System.out.println("Enter the expected average return on an investement: ");
+		System.out.println("Enter the expected average return while working: ");
 		double annual_return = user_input.nextDouble();
 		
-		System.out.println("Enter the amount of years retired : ");
-		int years_retired = user_input.nextInt();
+		System.out.println("Enter the amount of years retired: ");
+		double years_retired = user_input.nextDouble();
+		
+		System.out.println("Enter the expected average return while retired: ");
+		double retired_return = user_input.nextDouble();
 
 		System.out.println("Enter your required income: ");
 		double required_income = user_input.nextDouble();
@@ -26,18 +33,27 @@ public class Main {
 		double SSI_income = user_input.nextDouble();
 		
 		user_input.close();
-		double total_saving = total_saving(required_income, SSI_income, annual_return, years_to_work);
-		double save_monthly = save_monthly(total_saving, annual_return, years_to_work);
-		System.out.printf("%.2f%n",total_saving);
-		System.out.printf("%.2f%n",save_monthly);
+		
+		//Feed input into calculations and assign them to variables.
+		
+		double TS = total_saving(required_income, SSI_income, retired_return, years_to_work, years_retired);
+		double SM = save_monthly(TS, annual_return, years_to_work);
+		
+		//Print and format completed calculation variables.
+		
+		System.out.printf("You must save a total of, " + "$%.2f%n", TS);
+		System.out.printf("Each week you must save, " + "$%.2f%n", SM);
 	}
-	public static double total_saving(double required_income,double SSI_income,double annual_return,int years_to_work) {
-		double total_saving = (required_income-SSI_income)*((1-(1/(Math.pow(1+(annual_return/100)/12, years_to_work*12 )))))/((annual_return/100)/12);
-		return total_saving;
+	public static double total_saving(double required_income,double SSI_income,double retired_return,double years_to_work, double years_retired) 
+	{
+		//Calculate the total amount to save.
+		double TS = (required_income-SSI_income)*((1-(1/(Math.pow(1+(retired_return/100)/12, years_retired*12 )))))/((retired_return/100)/12);
+		return TS;
 	}
-	public static double save_monthly(double total_saving,double annual_return,int years_to_work) {
+	public static double save_monthly(double total_saving,double annual_return,double years_to_work) 
+	{
+		//Calculate the amounted needed to save per month.
 		double save_monthly = total_saving*(((annual_return/100)/12)/((Math.pow(1+(annual_return/100)/12, years_to_work*12))-1));
 		return save_monthly;
 	}
 }
-
